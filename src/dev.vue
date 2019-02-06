f889e610ca8b9721525a19432cc018c5f068c05f
Alec Troemel <alec@mirusresearch.com>
2019-02-06 14:45:30 -0600

    abstract dev out to parent component

---
+++ src/dev.vue
@@ -0,0 +1,35 @@
+<template>
+    <tiptapEditor
+        :value.sync="localtext"
+        :warnings="warnings"
+        placeholder="write something will-ya!"
+    />
+</template>
+
+<script>
+import tiptapEditor from './tiptap-editor.vue';
+
+export default {
+    components: { tiptapEditor },
+    data() {
+        return {
+            warnings: [
+                {
+                    value: 'the',
+                    message: 'did you mean...',
+                    options: ['too', 'pizza'], // optional
+                },
+                {
+                    value: 'test text',
+                    message: 'cannot say that, sorry',
+                    overrideClass: 'underlined-green', // optional
+                },
+            ],
+            localtext: 'this is the initial value',
+        };
+    },
+};
+</script>
+
+<style>
+</style>
