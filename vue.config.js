b4af8383b80f2f646eb9a69d24600afd5230e07e
Alec Troemel <alec@mirusresearch.com>
2019-03-20 15:20:09 -0500

    update build to actually work

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 module.exports = {
     css: { extract: false },
-    configureWebpack: () => ({
-        mode: 'production',
-        // plugins: [new BundleAnalyzerPlugin({ analyzerMode: 'static' })],
-    }),
-    chainWebpack: config => {
-        config.module.rule('vue').use('vue-loader');
-    },
 };
