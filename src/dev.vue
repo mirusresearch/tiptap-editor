6b480d6a01c459d562a634670e6c9782f3d65bf8
alec troemel <alec@mirusresearch.com>
2019-05-06 17:02:53 -0500

    style updates for dev server

--- src/dev.vue
+++ src/dev.vue
@@ -1,9 +1,11 @@
 <template>
-    <tiptapEditor
-    :value.sync="localtext"
-        :warnings="warnings"
-        placeholder="write something will-ya!"
-    />
+    <div>
+        <tiptapEditor
+            :value.sync="localtext"
+            :warnings="warnings"
+            placeholder="write something will-ya!"
+        />
+    </div>
 </template>
 
 <script>
@@ -44,5 +46,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
