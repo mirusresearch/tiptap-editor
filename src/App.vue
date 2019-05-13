92236d467fedf32d8566cfce534aff5ca4cd197a
alec troemel <alec@mirusresearch.com>
2019-05-13 16:16:58 -0500

    use vue cli server style dev

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <tiptapEditor
+        :value.sync="localtext"
+        :warnings="warnings"
+        placeholder="write something will-ya!"
+    />
+</template>
+
+<script>
+import tiptapEditor from './tiptap-editor.vue';
+
+export default {
+    name: 'app',
+    components: { tiptapEditor },
+    data() {
+        return {
+            warnings: [
+                {
+                    value: 'red',
+                    message: 'did you mean...',
+                    options: ['read', 'reed'], // optional
+                    // default class is red
+                },
+                {
+                    value: '<script&gt;', // can use the real thing, or escape it
+                    isWord: false,
+                    message: 'you sure you wanted a tag?',
+                    overrideClass: 'underline-blue', // optional
+                },
+                {
+                    value: 'green',
+                    message: 'I really like this!',
+                    overrideClass: 'underline-green', // optional
+                },
+                {
+                    value: 'orange',
+                    message: 'this is a bad idea!',
+                    overrideClass: 'underline-orange', // optional
+                },
+            ],
+            localtext:
+                '&lt;script&gt; welcome to tiptap editor, here is a red mark blue with suggestions. You can also to blue, orange, and green!!',
+        };
+    },
+};
+</script>
