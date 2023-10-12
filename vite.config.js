d3db02cc2feaae41747aa109fc51c5fb5e34df89
Jeremy Garey <jeremy@mirusresearch.com>
2023-10-12 07:13:48 -0400

    Fix vite build process

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
-
-const path = require('path');
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), cssInjectedByJsPlugin()],
     build: {
         lib: {
-            entry: path.resolve(__dirname, 'src/index.js'),
+            entry: resolve(__dirname, 'src/index.js'),
             name: 'TiptapEditor',
-            fileName: (format) => `tiptap-editor.${format}.js`,
+            fileName: 'tiptap-editor',
+            formats: ['es'],
         },
     },
 });
